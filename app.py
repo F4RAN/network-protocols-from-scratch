ae2e1b610a9e8b903715cc80d5c978ae4102494d
F4RAN <parsa.hassani30@gmail.com>
2024-02-10 07:12:08 +0330

    HTTP completed

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
-
 from http import HTTP
+
 request = HTTP()
 # Get Request
 # get_res = request.get("http://jsonplaceholder.typicode.com/todos/1", headers={"Accept": "application/json"})
 # print(get_res.json)
 
-# Post Request
-post_res = request.post("http://jsonplaceholder.typicode.com/posts", headers={"Content-Type": "application/json"}, data={"title": "foo", "body": "bar", "userId": 1})
-print(post_res.json)
+# Post Request post_res = request.post("http://jsonplaceholder.typicode.com/posts", headers={"Content-Type":
+# "application/json"}, data={"title": "foo", "body": "bar", "userId": 1}) print(post_res.json)
+
+# Put Request
+# put_res = request.put("http://jsonplaceholder.typicode.com/posts/1", headers={"Content-Type": "application/json"},
+#                       data={"title": "foo"})
+# print(put_res.json)
+
+# Patch Request
+# patch_res = request.patch("http://jsonplaceholder.typicode.com/posts/1", headers={"Content-Type": "application/json"},
+#                           data={"title": "foo"})
+# print(patch_res.json)
+
+
+
+
+# Delete Request
+# delete_res = request.delete("http://jsonplaceholder.typicode.com/posts/1")
+# print(delete_res.json)
--- http.py
+++ http.py
@@ -1,4 +1,4 @@
-# Based on RFC 2606, RFC 1035: https://datatracker.ietf.org/doc/html/rfc2616
+# Based on RFC 2606: https://datatracker.ietf.org/doc/html/rfc2616
 import json
 import socket
 import dns
@@ -58,12 +58,11 @@ class Request:
             chunk = self.s.recv(chunk_size)
 
         res = Response(header, response)
-        print(res.headers)
         total_length = int(res.headers["Content-Length"])
         remaining_bytes = total_length - len(response)
         chunk_count = (remaining_bytes // chunk_size) + 1 if remaining_bytes // chunk_size != 0 else 0
         for i in range(chunk_count):
-            print(f"Remaining bytes: {remaining_bytes}")
+            # print(f"Remaining bytes: {remaining_bytes}")
             chunk = self.s.recv(chunk_size)
             response += chunk
 
@@ -71,21 +70,71 @@ class Request:
         res.parse_body(response)
         return res
 
-
+# Repetitive in all http requests
+# Decorator to set up common HTTP request initialization steps
 def INITIATE_HTTP(func):
+    # Wrapper accepts original func arguments
     def wrapper(*args, **kwargs):
+        # Set empty headers dict if not passed
         if kwargs.get("headers") is None:
             kwargs["headers"] = ""
+
+        # Convert headers dict to HTTP format
         else:
             headers = [f"{k}: {v}\r\n" for k, v in kwargs["headers"].items()]
             kwargs["headers"] = "".join(headers)
+
+        # Get host and path from request URL
+        # vitalize.dev/hello/world
+        # host: vitalize.dev
+        # path: /hello/world
         host, path_params = args[0].make_host_from_url(args[1])
+
+        # Resolve host to IP address
         ip = args[0].resolve(host)
+
+        # Create socket
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        # Call the wrapped HTTP method
         return func(args[0], host, s, ip, path_params, **kwargs)
     return wrapper
 
+"""
+User-Agent - An HTTP header used to identify details about the client user agent like browser name and version. 
+    This helps servers identify client capabilities.
 
+Host - The domain name or IP address of the server that is being requested by the client.
+
+Accept - An HTTP header specifying content-types the client can understand like json, xml, html etc. 
+    Helps server determine best format to return.
+
+Authorization – Contains client credentials like username/password or tokens to gain access to protected resources.
+
+Cookie - Holds stateful session data that server can set with Set-Cookie header.
+    Sent back by client in subsequent requests.
+
+Content-Type – Media type of the request/response body like application/json, text/html etc. 
+    to allow correct parsing.
+
+Cache-Control – Directives like max-age, no-cache that specify how responses can be cached to improve performance.
+
+HTTP Request: Message sent from client to server containing method, headers, body - 
+    formatted according to HTTP standards. Methods like GET, POST define requested operation.
+
+HTTP Response: Message sent back from server to client in response to a request. 
+    Contains status code, headers and body of requested content or error messages.
+
+Methods:
+GET - The HTTP request method used to retrieve a representation of a resource.
+POST – Submit data to be processed to an identified resource. Often used to submit HTML form data to backend server.
+PUT – Upload/update a representation of a resource. Completely replaces existing resource.
+DELETE – Deletes the specified resource.
+OPTIONS – Used to describe communication options for target resource. Mainly used to retrieve supported methods, CORS settings etc.,
+PATCH – Apply partial modifications to a resource. Added later with RFC 5789.
+
+
+"""
 class HTTP:
     def __init__(self):
         self.user_agent = "f4ran-browser/1.0.0"
@@ -95,7 +144,7 @@ class HTTP:
         s.connect((ip, 80))
         get_request = f"GET /{path_params} HTTP/1.1\r\n" \
                       f"Host: {host}\r\n" \
-                      f"User-Agent: {self.user_agent}\r\n" \
+                      f"User-Agent: {self.user_agent}\r\n" \ 
                       f"Accept: */*\r\n" \
                       f"{headers if headers else ''}"  \
                       f"\r\n"
@@ -118,6 +167,51 @@ class HTTP:
         print(post_request)
         request = Request(s)
         response = request.send(post_request)
+        return response
+
+    @INITIATE_HTTP
+    def put(self, host, s, ip, path_params, data, headers):
+        s.connect((ip, 80))
+        data_string = json.dumps(data)
+        put_request = f"PUT /{path_params} HTTP/1.1\r\n" \
+                      f"Host: {host}\r\n" \
+                      f"User-Agent: {self.user_agent}\r\n" \
+                      f"Accept: */*\r\n" \
+                      f"Content-Length: {len(data_string)}\r\n" \
+                      f"{headers}" \
+                      f"\r\n" \
+                      f"{data_string if data else ''}"
+        request = Request(s)
+        response = request.send(put_request)
+        return response
+
+    @INITIATE_HTTP
+    def patch(self, host, s, ip, path_params, data, headers):
+        s.connect((ip, 80))
+        data_string = json.dumps(data)
+        patch_request = f"PATCH /{path_params} HTTP/1.1\r\n" \
+                        f"Host: {host}\r\n" \
+                        f"User-Agent: {self.user_agent}\r\n" \
+                        f"Accept: */*\r\n" \
+                        f"Content-Length: {len(data_string)}\r\n" \
+                        f"{headers}" \
+                        f"\r\n" \
+                        f"{data_string if data else ''}"
+        request = Request(s)
+        response = request.send(patch_request)
+        return response
+
+    @INITIATE_HTTP
+    def delete(self, host, s, ip, path_params, headers):
+        s.connect((ip, 80))
+        delete_request = f"DELETE /{path_params} HTTP/1.1\r\n" \
+                         f"Host: {host}\r\n" \
+                         f"User-Agent: {self.user_agent}\r\n" \
+                         f"Accept: */*\r\n" \
+                         f"{headers}" \
+                         f"\r\n"
+        request = Request(s)
+        response = request.send(delete_request)
         return response
 
     def resolve(self, host):
