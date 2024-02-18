8955a2f0707bd50b8a13f8027e30d582cb229cc5
F4RAN <parsa.hassani30@gmail.com>
2024-02-18 21:55:20 +0330

    key generation

--- tls_modules/tls.py
+++ tls_modules/tls.py
@@ -1,9 +1,15 @@
+import hashlib
+import hmac
 import re
 import socket
 import os
 import ssl
 import struct
+from math import ceil
+from random import randrange
+
 from OpenSSL import crypto
+
 # Generate a random 32 byte string for the client hello random field
 
 url = "example.org"
@@ -107,14 +113,14 @@ while len(tls_remaining) > 0:
         # Its server hello
         length = tls_remaining[6:9]
         version = tls_remaining[9:11]
-        random = tls_remaining[11:43]
+        server_hello_random = tls_remaining[11:43]
         session_id_length = tls_remaining[43:44]
         session_id = tls_remaining[44:76]
         cipher_suite = tls_remaining[76:78]
         compression_method = tls_remaining[78:79]
         tls_remaining = tls_remaining[79:]
         print(
-            f"Server hello: {version.hex()} {random.hex()} {session_id_length.hex()} {session_id.hex()} {cipher_suite.hex()} {compression_method.hex()}")
+            f"Server hello: {version.hex()} {server_hello_random.hex()} {session_id_length.hex()} {session_id.hex()} {cipher_suite.hex()} {compression_method.hex()}")
         continue
     if ht == 0x0b:
         # Its certificate
@@ -151,12 +157,11 @@ while len(certificates_remaining) > 0:
     certificates_list.append(certificate)
     certificates_remaining = certificates_remaining[3 + length:]
 
-
 # Load cert bytes into an X.509 object
 hostname = url if url.startswith("www.") else "www." + url
-for index,c in enumerate(certificates_list):
+prev_cert = ""
+for index, c in enumerate(certificates_list):
     cert = crypto.load_certificate(crypto.FILETYPE_ASN1, c)
-
     # Print some details from the cert
     print(f"Issuer: {cert.get_issuer()}")
     print(f"Subject: {cert.get_subject()}")
@@ -164,7 +169,6 @@ for index,c in enumerate(certificates_list):
     print(f"Version: {cert.get_version()}")
     print(f"Validity Start: {cert.get_notBefore()}")
     print(f"Validity End: {cert.get_notAfter()}")
-
     # Validate hostname matches
     if index == 0:
         common_name = cert.get_subject().commonName
@@ -173,14 +177,68 @@ for index,c in enumerate(certificates_list):
             exit(1)
     else:
         # Validate issuer matches previous cert subject
-        issuer = cert.get_issuer()
-        subject = crypto.load_certificate(crypto.FILETYPE_ASN1, certificates_list[index-1]).get_subject()
+        issuer = prev_cert.get_issuer()
+        subject = cert.get_subject()
         if issuer != subject:
             print(f"Issuer {issuer} does not match subject {subject}")
             exit(1)
+    prev_cert = cert
+
+p_len = int.from_bytes(server_key_exchange[:2], 'big')
+p = int.from_bytes(server_key_exchange[2:2 + p_len], 'big')
+server_key_exchange = server_key_exchange[2 + p_len:]
+g_len = int.from_bytes(server_key_exchange[:2], 'big')
+g = int.from_bytes(server_key_exchange[2:2 + g_len], 'big')
+server_key_exchange = server_key_exchange[2 + g_len:]
+pub_key_len = int.from_bytes(server_key_exchange[:2], 'big')
+pub_key = int.from_bytes(server_key_exchange[2:2 + pub_key_len], 'big')
+# print(f"Deffie-Hellman Server Params: p={p}, g={g}, pub_key={pub_key}")
+
+# Private key
+priv_key = randrange(0, p)
+public_key = pow(g, priv_key, p)
+pre_master_secret = pow(pub_key, priv_key, p)
+# convert to bytes
+pre_master_secret = pre_master_secret.to_bytes((pre_master_secret.bit_length() + 7) // 8, 'big')
+print(f"Pre-master secret: {pre_master_secret}")
+
+# Randoms
+c_random = client_hello_random
+s_random = server_hello_random
 
 
+# TLS PRF
+# PRF stands for "Pseudorandom Function". In the context of TLS, it is used to derive keys from secret seed material.
+# The TLS-PRF takes in a secret, a label, and a seed value. It then generates pseudorandom output by mixing the inputs,
+# usually with HMAC-SHA256 or a similar cryptographic hash.
+def TLS_PRF(secret, label, seed, output_size):
+    # Concatenate label and seed
+    label_seed = label + seed
+    # Generate output sized for keys
+    output_bytes = ceil(output_size / 8)  # Number of key bits /
+    # Initialize output
+    derived_output = b""
+    # MixSecret iterates hash multiple times
+    A = hmac.new(secret, msg=label_seed, digestmod=hashlib.sha256)
+    # hmac = HMAC-SHA256 in Py
+    i = 1
+    while len(derived_output) < output_bytes:
+        A.update(A.digest())
+        derived_output += A.digest()
+
+        i += 1
+
+        # Truncate to needed key size
+    return derived_output[:output_bytes]
 
 
+print(f"Pre master secret: {pre_master_secret.hex()}")
+print(f"Client random: {c_random.hex()}")
+print(f"Server random: {s_random.hex()}")
+master_secret = TLS_PRF(pre_master_secret, b"master secret", s_random + c_random, 48)
 
+# Generate key block
+key_block = TLS_PRF(master_secret, b"key expansion", s_random + c_random, 104)
 
+print(f"Master secret: {master_secret}")
+print(f"Key block: {key_block}")
